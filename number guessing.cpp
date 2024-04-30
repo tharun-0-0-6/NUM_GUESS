2b50868f3097566a1ec792ba8e6c8a50d38df69a
tharun-0-0-6 <167904525+tharun-0-0-6@users.noreply.github.com>
2024-05-01 00:01:13 +0530

    number_guessing.cpp

--- number guessing.cpp
+++ number guessing.cpp
@@ -4,91 +4,47 @@
 using namespace std;
 int main() 
 {
-	int randnum,diff,guess,lowlim,uplim;
+	int i,randnum,guess,lowlim,uplim;
 	cout<<"Enter the lower limit: ";
 	cin>>lowlim;
 	cout<<"Enter the upper limit: ";
 	cin>>uplim;
-	srand(time(0));
-	int range=uplim-lowlim+1;
-	randnum=(rand()%range)+lowlim;
-	cout<<"Enter the difficulty level (1/2/3) : ";
-	cin>>diff;
-	if(diff==1)
+	if(uplim>lowlim)
 	{
-		cout<<"You have THREE chances\n   First chance\n";
-		cout<<"Enter the number : ";
-		cin>>guess;
-		if(guess==randnum)
+		srand(time(0));
+		int range=uplim-lowlim+1;
+		randnum=(rand()%range)+lowlim;
+		cout<<"You have 5 chances\n";
+		for(i=0;i<5;i++)
 		{
-			cout<<"-----You have guessed the correct number-----";
-		}
-		else
-		{
-			cout<<".....The number you guessed is wrong.....\n  Second chance\n";
-			cout<<"Enter the number : ";
+			cout<<"Enter a number : ";
 			cin>>guess;
 			if(guess==randnum)
 			{
-				cout<<"-----You have guessed the correct number-----";
-			}
-			else
+				cout<<"You have guessed the correct number\n";
+				break;	
+			}	
+			else if((randnum-guess==10)||(randnum-guess==-10))
 			{
-				cout<<".....The number you guessed is wrong.....\n  Third chance\n";
-				cout<<"Enter the number : ";
-				cin>>guess;
-				if(guess==randnum)
-				{
-					cout<<"-----You have guessed the correct number-----";
-				}
-				else
-				{
-					cout<<".....The number you have guessed is wrong..... \nYour turn is over";
-					cout<<"\nThe correct number : "<<randnum;
-				}
+				cout<<"Your guess is too close\n";
+				if(i==4)
+					cout<<"The correct number : "<<randnum;
+			}
+			else if(randnum>guess)
+			{
+				cout<<"Your guess is lesser than the number\n";
+				if(i==4)
+					cout<<"The correct number : "<<randnum;
+			}
+			else if(randnum<guess)
+			{
+				cout<<"Your guess is higher than the number\n";
+				if(i==4)
+					cout<<"The correct number : "<<randnum;
 			}
 		}
 	}
-	else if(diff==2)
-	{
-		cout<<"You have TWO chances\n   First chance\n";
-		cout<<"Enter the number : ";
-		cin>>guess;
-		if(guess==randnum)
-		{
-			cout<<"-----You have guessed the correct number-----";
-		}
-		else
-		{
-			cout<<".....The number you guessed is wrong.....\n  Second chance\n";
-			cout<<"Enter the number : ";
-			cin>>guess;
-			if(guess==randnum)
-			{
-				cout<<"-----You have guessed the correct number-----";
-			}
-			else
-			{
-				cout<<".....The number you guessed is wrong.....\n  Your turn is over";
-				cout<<"\nThe correct number : "<<randnum;
-			}
-		}
-	}
-	else if(diff==3)
-	{
-		cout<<"You have ONE chance\n";
-		cout<<"Enter the number : ";
-		cin>>guess;
-		if(guess==randnum)
-		{
-			cout<<"-----You have guessed the correct number-----";
-		}
-		else
-		{
-			cout<<".....The number you guessed is wrong.....\n  Your turn is over";
-			cout<<"\nThe correct number : "<<randnum;
-		}
-	}
+	else
+		cout<<"Invalid input. Try again";
 	return 0;
 }
-
